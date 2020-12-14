f6f5ab287ef67c4e5cad210d5b6b51170cac1418
julgoncon <56026627+julgoncon@users.noreply.github.com>
2020-12-14 02:06:40 +0100

    Update telegram.py

--- bot/main/telegram.py
+++ bot/main/telegram.py
@@ -8,7 +8,7 @@ def generarText(recur,prod,cuenta,ataque):
     text=txt+cuenta+'====\n\nrecursos:\nmadera= '+str(recur[0])+'/'+str(recur[4][0])+'\nbarro= '+str(recur[1])+'/'+str(recur[4][0])+'\nhierro= '+str(recur[2])+'/'+str(recur[4][0])+'\ncereal= '+str(recur[3])+'/'+str(recur[4][1])+'\n\nproduccion:\nmadera= '+str(prod[0])+'\nbarro= '+str(prod[1])+'\nhierro= '+str(prod[2])+'\ncereal= '+str(prod[3])
     return text
 def enviarMensaje(text):
-    requests.post('https://api.telegram.org/bot1093492313:AAEm4yTb_1w8KaKVb-WPvZ5hsbj7Am6WNW0/sendMessage',
+    requests.post('https://api.telegram.org/[telegram-bot-token]/sendMessage',
               data={'chat_id': '-434698632', 'text': text})
 
 def enviarInformacionTelegram(cuenta,ataque):
